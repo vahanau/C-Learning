f3fc7a2559918083041f0802cc5b652509e52471
andrei.vahanau <a.vahanau@gmail.com>
2022-09-28 20:33:36 +0300

    Выполнил ДЗ 3

---
+++ Homework 3/DZ_Task 21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите X1: ");
+int x1 = int.Parse(ReadLine());
+Write("Введите Y1: ");
+int y1 = int.Parse(ReadLine());
+Write("Введите Z1: ");
+int z1 = int.Parse(ReadLine());
+Write("Введите X2: ");
+int x2 = int.Parse(ReadLine());
+Write("Введите Y2: ");
+int y2 = int.Parse(ReadLine());
+Write("Введите Z2: ");
+int z2 = int.Parse(ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+WriteLine($"Длина отрезка = {d:f3} ");
